e85bdc257ad5faa2fcbc399e153818417d3bb43e
Ruud Kamphuis <ruudk@mphuis.com>
2011-09-21 15:36:47 +0200

    Using isvat.appspot.com temporally

--- Service/VATService.php
+++ Service/VATService.php
@@ -2,6 +2,7 @@
 
 namespace Sparkling\VATBundle\Service;
 
+use Sparkling\VATBundle\Exception\VATException;
 use Sparkling\VATBundle\Exception\InvalidCountryCodeException;
 use Sparkling\VATBundle\Exception\InvalidVATNumberException;
 
@@ -9,12 +10,20 @@ class VATService
 {
 	public function validate($countryCode, $vatNumber)
 	{
+		$countryCode = preg_replace('/[^a-zA-Z]/', '', $countryCode);
+		$vatNumber = preg_replace('/[^a-zA-Z0-9]/', '', $vatNumber);
+
 		if(!preg_match('/^[A-Z]{2}$/', $countryCode))
 			throw new InvalidCountryCodeException('The countrycode is not valid. It must be in format [A-Z]{2}');
 
 		if(!preg_match('/^[0-9A-Za-z\+\*\.]{2,12}$/', $vatNumber))
 			throw new InvalidVATNumberException('The VAT number is not valid. It must be in format [0-9A-Za-z\+\*\.]{2,12}');
 
+		return $this->checkWithAppspot($countryCode, $vatNumber);
+	}
+
+	protected function checkWithVIES($countryCode, $vatNumber)
+	{
 		try
 		{
 			ini_set("soap.wsdl_cache_enabled", 0);
@@ -24,7 +33,7 @@ class VATService
 				'style'         => SOAP_DOCUMENT,
 				'encoding'      => SOAP_LITERAL,
 				'location'      => 'http://ec.europa.eu/taxation_customs/vies/services/checkVatService',
-				'trace' => 1
+				'trace'         => 1
 			));
 
 			$result = $client->checkVat(array(
@@ -38,5 +47,21 @@ class VATService
 		{
 			throw new VATException($exception);
 		}
+	}
+
+	protected function checkWithAppspot($countryCode, $vatNumber)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+		curl_setopt($ch, CURLOPT_URL, 'http://isvat.appspot.com/' . addslashes($countryCode) . '/' . addslashes($vatNumber) . '/'); // set url to post to
+		curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		return $result == 'true';
 	}
 }
