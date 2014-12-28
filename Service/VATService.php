01da0ec3ccf6b4355e2cfb27d9c338b3f0e90790
kallosz <kkallosz@gmail.cm>
2014-12-28 13:08:47 +0100

    add gitignore. fix psr2

--- Service/VATService.php
+++ Service/VATService.php
@@ -2,14 +2,50 @@
 
 namespace Sparkling\VATBundle\Service;
 
-use Sparkling\VATBundle\Exception\VATException;
 use Sparkling\VATBundle\Exception\InvalidCountryCodeException;
 use Sparkling\VATBundle\Exception\InvalidVATNumberException;
+use Sparkling\VATBundle\Exception\VATException;
 
 class VATService
 {
-    static $validCountries = array('AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 'GB', 'GR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK');
+    protected static $validCountries = array(
+        'AT',
+        'BE',
+        'BG',
+        'CY',
+        'CZ',
+        'DE',
+        'DK',
+        'EE',
+        'ES',
+        'FI',
+        'FR',
+        'GB',
+        'GR',
+        'HU',
+        'IE',
+        'IT',
+        'LT',
+        'LU',
+        'LV',
+        'MT',
+        'NL',
+        'PL',
+        'PT',
+        'RO',
+        'SE',
+        'SI',
+        'SK'
+    );
 
+    /**
+     * @param $countryCode
+     * @param null $vatNumber
+     * @return bool
+     * @throws InvalidCountryCodeException
+     * @throws InvalidVATNumberException
+     * @throws VATException
+     */
     public function validate($countryCode, $vatNumber = null)
     {
         if (!isset($vatNumber)) {
@@ -20,47 +56,67 @@ class VATService
         $countryCode = preg_replace('/[^a-zA-Z]/', '', $countryCode);
         $vatNumber = preg_replace('/[^a-zA-Z0-9]/', '', $vatNumber);
 
-        if(!preg_match('/^[A-Z]{2}$/', $countryCode))
-            throw new InvalidCountryCodeException('The countrycode is not valid. It must be in format [A-Z]{2}');
+        if (!preg_match('/^[A-Z]{2}$/', $countryCode)) {
+            throw new InvalidCountryCodeException('The country code is not valid. It must be in format [A-Z]{2}');
+        }
 
-        if(!in_array($countryCode, self::$validCountries))
-            throw new InvalidCountryCodeException('The countrycode is not valid. It must be one of '.implode(', ', self::$validCountries));
+        if (!in_array($countryCode, self::$validCountries)) {
+            throw new InvalidCountryCodeException(
+                'The country code is not valid. It must be one of ' . implode(', ', self::$validCountries)
+            );
+        }
 
-        if(!preg_match('/^[0-9A-Za-z\+\*\.]{2,12}$/', $vatNumber))
-            throw new InvalidVATNumberException('The VAT number is not valid. It must be in format [0-9A-Za-z\+\*\.]{2,12}');
+        if (!preg_match('/^[0-9A-Za-z\+\*\.]{2,12}$/', $vatNumber)) {
+            throw new InvalidVATNumberException(
+                'The VAT number is not valid. It must be in format [0-9A-Za-z\+\*\.]{2,12}'
+            );
+        }
 
         return $this->checkWithVIES($countryCode, $vatNumber);
     }
 
+    /**
+     * @param $countryCode
+     * @param $vatNumber
+     * @return bool
+     * @throws VATException
+     */
     protected function checkWithVIES($countryCode, $vatNumber)
     {
         try {
             ini_set("soap.wsdl_cache_enabled", 0);
 
-            $client = new \SoapClient(__DIR__.'/../Resources/wsdl/checkVatService.wsdl', array(
-                'soap_version'  => SOAP_1_1,
-                'style'         => SOAP_DOCUMENT,
-                'encoding'      => SOAP_LITERAL,
-                'location'      => 'http://ec.europa.eu/taxation_customs/vies/services/checkVatService',
-                'trace'         => 1
+            $client = new \SoapClient(__DIR__ . '/../Resources/wsdl/checkVatService.wsdl', array(
+                'soap_version' => SOAP_1_1,
+                'style' => SOAP_DOCUMENT,
+                'encoding' => SOAP_LITERAL,
+                'location' => 'http://ec.europa.eu/taxation_customs/vies/services/checkVatService',
+                'trace' => 1
             ));
 
             $result = $client->checkVat(array(
-                'countryCode'   => $countryCode,
-                'vatNumber'     => $vatNumber
+                'countryCode' => $countryCode,
+                'vatNumber' => $vatNumber
             ));
 
-            return $result->valid ?  true : false;
+            return $result->valid ? true : false;
         } catch (\SoapFault $exception) {
             throw new VATException($exception);
         }
     }
 
+    /**
+     * @param $countryCode
+     * @param $vatNumber
+     * @return bool
+     */
     protected function checkWithAppspot($countryCode, $vatNumber)
     {
+        $url = 'http://isvat.appspot.com/' . addslashes($countryCode) . '/' . addslashes($vatNumber) . '/';
+        
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-        curl_setopt($ch, CURLOPT_URL, 'http://isvat.appspot.com/' . addslashes($countryCode) . '/' . addslashes($vatNumber) . '/'); // set url to post to
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_FAILONERROR, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
